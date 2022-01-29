312bb5d7a888ecdadd2f40df9d8cef207b245ac4
Lucas Rodrigues <lucax.99@outlook.com>
2022-01-29 12:32:02 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthContextProvider } from './contexts/AuthContexts'
 
-function App() {
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
+import { Room } from './pages/Room';
+
+export function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <AuthContextProvider>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms/new"  element={<NewRoom />} />
+          <Route path="/rooms/:id" element={<Room />} />
+      </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
+
   );
 }
 
-export default App;
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,70 @@
+import { FormEvent, useState } from 'react'
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+
+import '../styles/auth.scss'
+
+import { Button } from '../components/Button'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
+
+
+
+
+export function NewRoom() {
+
+    const { user } = useAuth()
+
+    const history = useNavigate()
+
+    const [newRoom, setNemRoom] = useState('')
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault()
+
+        if(newRoom.trim() === '') {
+            return
+        }
+
+        const roomRef = database.ref('rooms')
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id
+        })
+
+        history(`/rooms/${firebaseRoom.key}`)
+    }
+
+    
+    return (
+        <div id='page-auth'>
+            <aside>
+                <img src={illustrationImg} alt='Imagem ilustrando perguntas e respostas' />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo real</p>
+            </aside>
+            <main>
+
+                <div className='main-content'>
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Criar uma nova sala</h2>
+                    <form onSubmit={handleCreateRoom}>
+                        <input
+                            type="text" 
+                            placeholder='Nome da sala' 
+                            onChange={event => setNemRoom(event.target.value) }
+                            value={newRoom}
+                        />
+                        <Button type="submit">Criar sala</Button>
+                    </form>
+                    <p>
+                        Quer entrar em uma sala? <Link to="/">Clique aqui</Link>
+                    </p>
+                </div>
+            </main>
+        </div>
+    );
+}
